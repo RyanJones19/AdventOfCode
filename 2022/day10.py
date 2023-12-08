5566a09c713df3f22cb68a27c32b27c0f0e67ccc
rjo138_nike <ryan.jones@nike.com>
2023-12-07 19:39:34 -0800

    lol very unreadable

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,28 +1,17 @@
 import sys
 machineInstructions = open(sys.argv[1]).read().strip().split('\n')
 class Machine:
-    def __init__(self):
-        self.X = 1
-        self.clock = 0
-        self.clockMap: list(tuple(int, int)) = []
-        self.spritePosition = range(3)
-        self.CRTScreen = [" " for _ in range(240)]
+    def __init__(self): self.X, self.clock, self.clockMap, self.spritePosition, self.CRTScreen = 1, 0, [], range(3), [" " for _ in range(240)]
     def operate(self, operation: str):
         if operation == "noop":
             self.perform_cycle()
         else:
             operation, value = operation.split()
-            self.perform_cycle()
-            self.perform_cycle()
+            self.perform_cycle(), self.perform_cycle()
             self.X += int(value)
             self.spritePosition = range((self.X-1)%40, (self.X+2)%40)
-    def perform_cycle(self):
-        self.draw()
-        self.clock += 1
-        self.clockMap.append((self.clock, self.X))
-    def draw(self):
-        if (self.clock%40) in self.spritePosition:
-            self.CRTScreen[self.clock] = "#"
+    def perform_cycle(self): self.draw(); self.clock += 1; self.clockMap.append((self.clock, self.X))
+    def draw(self): self.CRTScreen[self.clock] = "#" if self.clock%40 in self.spritePosition else " "
 machine = Machine()
 [machine.operate(instruction) for instruction in machineInstructions]
 signalStrengths = [cycle[0] * cycle[1] for cycle in machine.clockMap if cycle[0] in [20,60,100,140,180,220]]
